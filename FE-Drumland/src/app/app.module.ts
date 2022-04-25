cc2d4bd5cbfc9c699b66ff9568fadf36f4ab58fd
sergiogonzalez24 <sergiomanuel2020@gmail.com>
2022-04-25 08:14:56 -0500

    ranking preview

--- FE-Drumland/src/app/app.module.ts
+++ FE-Drumland/src/app/app.module.ts
@@ -3,18 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenubarComponent } from './menubar/menubar.component';
-import { HomeContentComponent } from './home-content/home-content.component';
+import { MenubarComponent } from './components/menubar/menubar.component';
+import { HomeContentComponent } from './components/home-content/home-content.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { RankingComponent } from './components/ranking/ranking.component';
+import { SearchComponent } from './components/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenubarComponent,
-    HomeContentComponent
+    HomeContentComponent,
+    FooterComponent,
+    LoginComponent,
+    UserComponent,
+    RankingComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeContentComponent },
+      { path: 'ranking', component: RankingComponent },
+      { path: 'search', component: SearchComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'profile/:username', component: UserComponent },
+
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
